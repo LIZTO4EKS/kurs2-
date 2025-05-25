714383d4f7bcd8ba39d01c76342bb82638e35355
LIZTO4EKS <ivantolkachev555@mail.ru>
2025-05-25 15:30:33 +0500

    добавил возможность удаления заметок в корзину а из корзины возможность их удаления

--- notes/urls.py
+++ notes/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.note_list, name='note_list'),
     path('toggle/<int:pk>/', views.note_toggle_done, name='note_toggle_done'),
+    path('delete/<int:pk>/', views.note_delete, name='note_delete'),
+    path('trash/', views.trash, name='trash'),
+    path('restore/<int:pk>/', views.restore_note, name='restore_note'),
+    path('permanently_delete/<int:pk>/', views.permanently_delete_note, name='permanently_delete_note'),
     path('signup/', views.signup, name='signup'),
 ]
--- notes/views.py
+++ notes/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth import login
 
 @login_required
 def note_list(request):
-    notes = Note.objects.filter(user=request.user).order_by('due_date')
+    notes = Note.objects.filter(user=request.user, is_deleted=False).order_by('due_date')
     if request.method == 'POST':
         form = NoteForm(request.POST)
         if form.is_valid():
@@ -19,12 +19,42 @@ def note_list(request):
         form = NoteForm()
     return render(request, 'notes/note_list.html', {'notes': notes, 'form': form})
 
+
 @login_required
 def note_toggle_done(request, pk):
     note = get_object_or_404(Note, pk=pk, user=request.user)
     note.is_done = not note.is_done
     note.save()
     return redirect('note_list')
+
+
+@login_required
+def note_delete(request, pk):  # <--- Функция для удаления в корзину
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    note.is_deleted = True
+    note.save()
+    return redirect('note_list')
+
+
+@login_required
+def trash(request):  # Корзина
+    deleted_notes = Note.objects.filter(user=request.user, is_deleted=True)
+    return render(request, 'notes/trash.html', {'deleted_notes': deleted_notes})
+
+
+@login_required
+def restore_note(request, pk):  # Восстановить из корзины
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    note.is_deleted = False
+    note.save()
+    return redirect('trash')
+
+
+@login_required
+def permanently_delete_note(request, pk):  # Полное удаление
+    note = get_object_or_404(Note, pk=pk, user=request.user)
+    note.delete()
+    return redirect('trash')
 
 
 def signup(request):
